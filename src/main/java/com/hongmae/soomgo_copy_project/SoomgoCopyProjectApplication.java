bd9b49bd1068a4310a3debe7569e0a6c794f1d5b
Colin Park (Hanbin Park) <qkrgksqls9@gmail.com>
2024-03-08 05:26:49 +0900

    Project init
    1. Add Spring Boot Project (dir. /)
    2. Add React Project (dir. src/main/frontend)

---
+++ src/main/java/com/hongmae/soomgo_copy_project/SoomgoCopyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hongmae.soomgo_copy_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoomgoCopyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoomgoCopyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongmae/soomgo_copy_project/SoomgoCopyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongmae.soomgo_copy_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoomgoCopyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
